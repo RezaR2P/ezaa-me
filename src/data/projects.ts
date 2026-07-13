bbad29df255ff06d1e1e45a9a2c703718ebb1986
RezaR2P <kecilreza575@gmail.com>
2026-07-13 13:33:42 +0700

    feat: halaman projects dengan ProjectCard dan icon

---
+++ src/data/projects.ts
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: "Sistem Parkir Universitas IPWIJA",
+    description:
+      "Sistem Parkir Universitas IPWIJA adalah sebuah aplikasi berbasis Python untuk mengelola parkir kendaraan di lingkungan kampus.",
+    github: "https://github.com/RezaR2P/parkir_sistem",
+    image: "/img/project/parkir.avif",
+    stacks: ["Python"],
+  },
+];
